03cc19f3c16a3226d152360077ae2404e437fcb2
ZhouZaihang <1085212484@qq.com>
2018-08-17 23:48:36 +0800

    finish code

---
+++ code/LED6432_esp32_32/Release/core/cbuf.cpp.d
@@ -0,0 +1,20 @@
+C:\Users\zzh_8\AppData\Local\Temp\VMBuilds\LED6432_esp32_32\espressif_esp32\Release\core\cbuf.cpp.o: \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32\cores\esp32\cbuf.cpp \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32\cores\esp32\cbuf.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdint.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_default_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/features.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_intsup.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/string.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/_ansi.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/newlib.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/config.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/ieeefp.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/esp32/xtensa/config/core-isa.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/reent.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/_ansi.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/lock.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/cdefs.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/string.h
---
+++ code/LED6432_esp32_32/Release/core/stdlib_noniso.c.d
@@ -0,0 +1,24 @@
+C:\Users\zzh_8\AppData\Local\Temp\VMBuilds\LED6432_esp32_32\espressif_esp32\Release\core\stdlib_noniso.c.o: \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32\cores\esp32\stdlib_noniso.c \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdlib.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/ieeefp.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/_ansi.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/newlib.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/config.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/features.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/esp32/xtensa/config/core-isa.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/reent.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/_ansi.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_default_types.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/lock.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/cdefs.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/stdlib.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/alloca.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/string.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/string.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdint.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_intsup.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/math.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\espressif\esp32\cores\esp32\stdlib_noniso.h
